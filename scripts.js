8efca104922e06155939b0e4179a5ef1915bb9f4
Alex Wilson <alexwilsonlearn@gmail.com>
2020-11-29 08:36:35 -0600

    Added 16x16 square divs in JS file. Added styling to eliminate gaps between divs and set borders

--- scripts.js
+++ scripts.js
@@ -1,3 +1,14 @@
 // Main Container from Index
-let container = document.querySelector('.container');
+const container = document.querySelector('.container');
 
+
+function makeRows(rows){
+    for (i = 0; i < rows; i++){
+        let row = document.createElement('div');
+        container.appendChild(row).className='gridRow';
+    }
+    }
+
+
+
+    makeRows(256);
