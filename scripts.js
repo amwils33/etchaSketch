550faa4d8d56f90b2c6ae34d39df696b7d63e9d3
Alex Wilson <alexwilsonlearn@gmail.com>
2020-12-01 17:57:53 -0600

    Added upper limit to newGrid prompt so it will not go over 100. Issue logged for square style/width issues when dealing with odd grid numbers

--- scripts.js
+++ scripts.js
@@ -1,9 +1,8 @@
 const container = document.querySelector('.container');
+container.style.width = "600px";
+container.style.height = "600px";
 const reset = document.querySelector('.reset');
-reset.onclick = () => clearGrid();
-
-
-
+reset.onclick = () => clearGrid(); 
 
 // Create Grid
 function makeGrid(x){
@@ -14,22 +13,27 @@ for (let i = 0; i < x; i++){
         square.className = 'square';
         container.appendChild(square);   
 
-square.onmouseover = makeBlack;
-function makeBlack(){
-square.style.backgroundColor = 'black';
-}
+        //Maybe try adjusting these to be percentages?
+        square.style.width = 600 / x + 'px';
+        square.style.height = 600 / x + 'px';
 
-}
-}
-}
+ square.onmouseover = makeBlack;
+        function makeBlack (){
+            square.style.backgroundColor = 'black';
+        }
+
+}}}
 
 makeGrid(16);
 
-//
+//Cleared and creates new grid and prompts for number of squares per side. If over 100 it will keep prompting.
 function clearGrid(){
     while (container.firstChild){
         container.firstChild.remove()
     }
-    let newGrid = prompt('Please tell me how many squares you want on each side')
+    let newGrid = prompt('Please tell me how many squares you want on each side. 100 or less please')
+        while(newGrid > 100){
+            newGrid = prompt('Please enter a number that is 100 or lower')
+        }
     makeGrid(newGrid);
 }
