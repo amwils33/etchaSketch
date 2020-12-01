620c16850f54c866c8323ef4210b2b88e0d0e131
Alex Wilson <alexwilsonlearn@gmail.com>
2020-12-01 06:39:21 -0600

    Added reset button to clear grid and make new grid. Adjusted styling of individual squares

--- scripts.js
+++ scripts.js
@@ -1,20 +1,35 @@
 const container = document.querySelector('.container');
+const reset = document.querySelector('.reset');
+reset.onclick = () => clearGrid();
 
 
-for (let i = 0; i < 16; i++){
-    for(let j = 0; j < 16; j++){
+
+
+// Create Grid
+function makeGrid(x){
+    
+for (let i = 0; i < x; i++){
+    for(let j = 0; j < x; j++){
         let square = document.createElement('div');
         square.className = 'square';
-        container.appendChild(square);
-
+        container.appendChild(square);   
 
 square.onmouseover = makeBlack;
-
 function makeBlack(){
-    square.style.backgroundColor = 'black';
+square.style.backgroundColor = 'black';
+}
+
 }
 }
 }
 
+makeGrid(16);
 
-
+//
+function clearGrid(){
+    while (container.firstChild){
+        container.firstChild.remove()
+    }
+    let newGrid = prompt('Please tell me how many squares you want on each side')
+    makeGrid(newGrid);
+}
